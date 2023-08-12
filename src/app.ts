562910bfb9ec5bbcc34ebc8f357d4225046b4323
khatrisagar <khatrisagar404@gmail.com>
2023-08-12 12:29:47 +0530

    initial commit

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,19 @@
 import { Client, GatewayIntentBits } from "discord.js";
+import axios from "axios";
 const client: any = new Client({ intents: [GatewayIntentBits.Guilds] });
 require("dotenv").config();
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
+
+setInterval(async () => {
+  try {
+    await axios.get(`${process.env.BACKEND_URL}`);
+    console.log("pinged");
+  } catch (error) {
+    console.log("error", error);
+  }
+}, 598888);
 
 client.login(process.env.DISCORD_TOKEN);
